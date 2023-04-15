b9f413773fe89dcdce285e2410e3b0edfe8b5749
Rafael Angelo <angelo.rafael.ti@gmail.com>
2023-04-15 00:00:38 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 const buttonNewTask = document.querySelector('.new-task button');
 const taskBox = document.querySelector('.task-box');
 const newTaskInput = document.querySelector('.new-task input');
-const deleteTaskButton = document.querySelector('.task-item button');
 
 buttonNewTask.addEventListener('click', addTask);
-deleteTaskButton.addEventListener('click', deleteTask);
 
 function addTask() {
-    
-    console.log(newTaskInput.value)   
     event.preventDefault();
 
     const taskItem = document.createElement('div');
     taskItem.classList.add('task-item');
 
-    const inputItem = document.createElement('input');
-    inputItem.type = 'checkbox';
+    const checkboxInput = document.createElement('input');
+    checkboxInput.type = 'checkbox';
 
     const taskItemText = document.createElement('p');
     taskItemText.innerText = newTaskInput.value;
 
     const buttonTrash = document.createElement('button');
-    
+
     const trashItem = document.createElement('i');
     trashItem.classList.add('fa');
     trashItem.classList.add('fa-trash');
 
-    taskItem.appendChild(inputItem);
+    taskItem.appendChild(checkboxInput);
     taskItem.append(taskItemText);
     taskItem.appendChild(buttonTrash);
     buttonTrash.appendChild(trashItem);
     taskBox.appendChild(taskItem);
+
+    trashItem.addEventListener('click', deleteTask);
+    checkboxInput.addEventListener('click', completeTask);
+
     newTaskInput.value = '';
 };
 
-function deleteTask (e){
-    console.log(e);
+function deleteTask(e) {
     e.target.parentElement.parentElement.remove();
 };
 
